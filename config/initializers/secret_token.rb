a456b7f7e76b915ca129a60eec4d2c2de722a11f
Lauren Yeiser & Robin Miller <student@epicodus.com>
2014-04-11 09:03:21 -0700

    is the first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StopAndShop::Application.config.secret_key_base = 'a62ef02a5168b14f80daef72b85a5190d9331f3bcca9ab89e5a97c870a8f74a2c21fd9fa44c28b6041c5bdfc7fc014db7e09bdc431a279aa4bd86e38bf1288c4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StopAndShop::Application.config.session_store :cookie_store, key: '_stop_and_shop_session'
